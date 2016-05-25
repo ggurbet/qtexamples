baa436ea45ad8f621b9eade5d8585a1b368d1004
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-25 17:20:18 +0300

    Example 012

---
+++ qt012/main.cpp
@@ -0,0 +1,48 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QString>
+#include <QDebug>
+#include <QTextStream>
+
+void Write(QString Filename)
+{
+    QFile mFile(Filename);
+    if (!mFile.open(QFile::WriteOnly | QFile::Text))
+    {
+        qDebug() << "Cannot open file for writing";
+        return;
+    }
+
+    QTextStream out(&mFile);
+    out << "Hello world!";
+    mFile.flush();
+    mFile.close();
+}
+
+void Read(QString Filename)
+{
+    QFile mFile(Filename);
+    if (!mFile.open(QFile::ReadOnly | QFile::Text))
+    {
+        qDebug() << "Cannot open file for reading";
+        return;
+    }
+
+    QTextStream in(&mFile);
+    QString mText = in.readAll();
+
+    qDebug() << mText;
+
+    mFile.close();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QString mFilename = "gokhan.txt";
+    Write(mFilename);
+    Read(mFilename);
+
+    return a.exec();
+}
