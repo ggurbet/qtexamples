643e2835e3b1453d3f57c5ba7fad8b3a652817f7
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-26 15:01:26 +0300

    Finished example 23

--- qt023/mainwindow.cpp
+++ qt023/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QMessageBox::information(this, "Title", "Hello");
 }
