f50b5e02d3a4b7095b18cca114c9cc4cc03c024c
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-30 15:18:15 +0300

    Finished example 26

--- qt026/dialog.cpp
+++ qt026/dialog.cpp
@@ -1,5 +1,6 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QMessageBox>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +12,44 @@ Dialog::Dialog(QWidget *parent) :
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    // info
+    QMessageBox::information(this, "Information", "It works!");
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    // question
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(this, "Question", "Do you like cats?",
+                                  QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+        QMessageBox::information(this, "Information", "You like cats!");
+    else
+        QMessageBox::information(this, "Information", "You don't like cats!");
+}
+
+void Dialog::on_pushButton_3_clicked()
+{
+    // warning
+    QMessageBox::warning(this, "Warning", "Be warned");
+}
+
+void Dialog::on_pushButton_4_clicked()
+{
+    // custom
+    QMessageBox::StandardButton reply;
+    reply = QMessageBox::question(this, "Custom", "My text here",
+                                  QMessageBox::Yes | QMessageBox::YesToAll | QMessageBox::No | QMessageBox::NoToAll);
+    if (reply == QMessageBox::Yes)
+        QMessageBox::information(this, "Information", "You said yes!");
+    else if (reply == QMessageBox::YesToAll)
+        QMessageBox::information(this, "Information", "You said yes to all!");
+    if (reply == QMessageBox::No)
+        QMessageBox::information(this, "Information", "You said no!");
+    else if (reply == QMessageBox::NoToAll)
+        QMessageBox::information(this, "Information", "You said no to all!");
 }
--- qt026/dialog.h
+++ qt026/dialog.h
@@ -15,6 +15,15 @@ public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::Dialog *ui;
 };
