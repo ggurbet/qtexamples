31e2531bf21497630ed101827d64277ab8b78599
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-30 17:15:48 +0300

    Modified gitignore to ignore all backups

---
+++ qt031/mythread.cpp
@@ -0,0 +1,6 @@
+#include "mythread.h"
+
+MyThread::MyThread()
+{
+
+}
---
+++ qt031/mythread.h
@@ -0,0 +1,11 @@
+#ifndef MYTHREAD_H
+#define MYTHREAD_H
+
+
+class MyThread : public QThread
+{
+public:
+    MyThread();
+};
+
+#endif // MYTHREAD_H
