9b9f06f3684d664b252e21b4585708c59979a69a
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-30 16:49:52 +0300

    Finished example 30

--- qt030/mythread.cpp
+++ qt030/mythread.cpp
@@ -12,6 +12,14 @@ void MyThread::run()
 
     for (unsigned int i = 0; i < 1000; i++)
     {
+        QMutex mutex;
+        mutex.lock();
+        if (this->Stop)
+        {
+            break;
+        }
+        mutex.unlock();
+        this->sleep(1);
         qDebug() << this->name << i;
     }
 }
--- qt030/mythread.h
+++ qt030/mythread.h
@@ -2,6 +2,7 @@
 #define MYTHREAD_H
 
 #include <QThread>
+#include <QMutex>
 
 class MyThread : public QThread
 {
@@ -9,6 +10,7 @@ public:
     MyThread();
     void run();
     QString name;
+    bool Stop;
 };
 
 #endif // MYTHREAD_H
