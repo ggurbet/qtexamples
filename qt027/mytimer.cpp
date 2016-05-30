b82b563bcf1b4fe82a22d4cab5cb6c041d9c904f
Gökhan Gurbetoğlu <ggurbet@gmail.com>
2016-05-30 15:33:23 +0300

    Making timer ready

--- qt027/mytimer.cpp
+++ qt027/mytimer.cpp
@@ -2,5 +2,13 @@
 
 MyTimer::MyTimer()
 {
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(MySlot()));
+
+    timer->start(1000);
+}
+
+void MyTimer::MySlot()
+{
 
 }
--- qt027/mytimer.h
+++ qt027/mytimer.h
@@ -1,11 +1,17 @@
 #ifndef MYTIMER_H
 #define MYTIMER_H
 
+#include <QTimer>
 
-class MyTimer
+class MyTimer : public QObject
 {
+    Q_OBJECT
+
 public:
     MyTimer();
+    QTimer *timer;
+public slots:
+    void MySlot();
 };
 
 #endif // MYTIMER_H
